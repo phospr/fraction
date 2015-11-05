b38a90f6a7c1655c73e38ce2a96641c3e0835564
Tom Haskins-Vaughan <tomhv@janeiredale.com>
2015-11-05 16:07:10 -0500

    Added exceptions

---
+++ src/Exception/Fraction/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Phospr Fraction package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phospr\Exception\Fraction;
+
+/**
+ * Exception class thrown when an invalid argument is passed
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  0.1.0
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
